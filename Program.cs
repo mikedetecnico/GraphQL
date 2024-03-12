9a3275cf4d184a2f6356509c8a385f8ac12ebf5a
Michael Graessle <michael.graessle@gmail.com>
2024-03-12 16:15:21 -0700

    registering necessary services

--- Program.cs
+++ Program.cs
@@ -1,14 +1,52 @@
+using GraphiQl;
+using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Data;
+using GraphQLProject.Mutations;
+using GraphQLProject.Queries;
+using GraphQLProject.Repositories;
+using GraphQLProject.Schema;
+using GraphQLProject.Types;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
+// types
+builder.Services.AddTransient<PatientInputType>();
+builder.Services.AddTransient<StudyInputType>();
+builder.Services.AddTransient<PatientType>();
+builder.Services.AddTransient<StudyType>();
+
+// repositories
+builder.Services.AddTransient<IPatientRepository, PatientRepository>();
+builder.Services.AddTransient<IStudyRepository, StudyRepository>();
+
+// queries
+builder.Services.AddTransient<PatientQuery>();
+builder.Services.AddTransient<StudyQuery>();
+builder.Services.AddTransient<RootQuery>();
+
+// mutations
+builder.Services.AddTransient<PatientMutation>();
+builder.Services.AddTransient<StudyMutation>();
+builder.Services.AddTransient<RootMutation>();
+
+// schema
+builder.Services.AddTransient<ISchema, RootSchema>();
+builder.Services.AddGraphQL(b => b.AddAutoSchema<ISchema>().AddSystemTextJson());
+
+// db connection
+builder.Services.AddDbContext<GraphQLDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GraphQLDbContextConnection")));
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseGraphiQl("/graphql");
+app.UseGraphQL<ISchema>();
 
 app.UseAuthorization();
 
